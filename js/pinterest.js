dead6af7447a8a8cbc64fc1a1a951dc92f25e02b
YipGiH <yenigomez2205@gmail.com>
2023-09-25 07:06:34 -0500

    Cambios

---
+++ js/pinterest.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var imagenDiv = document.getElementById("imagen1");
+
+    imagenDiv.addEventListener("click", function(){
+        window.location.href = "pinterest-img-1.html";
+    });
+});
