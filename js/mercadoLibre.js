696aa7c5d6029b23d43e6a51f8cec8a917fcdce5
YipGiH <yenigomez2205@gmail.com>
2023-10-16 17:18:25 -0500

    Nuevo Portafolio updates

--- js/mercadoLibre.js
+++ js/mercadoLibre.js
@@ -6,15 +6,37 @@ class Automovil{
         this.marca = marca;
         this.modelo = modelo;
     }
-}
-
+};
 let auto1 = new Automovil("Pagani","Huayra");
 
-alert(auto1.marca + " " +auto1.modelo);
+//alert(auto1.marca + " " +auto1.modelo);
 
-inputBusqueda.addEventListener("keydown", function(){
+/* inputBusqueda.addEventListener("keydown", function(event){ */
+window.addEventListener("load", function(event){
+    /* if(event.key=="Enter"){ */
     let mainContent = document.getElementById("mainContent");
+
+    /* Caja para el producto */
     let boxProducto = document.createElement("div");
-    mainContent.appenChild(boxProducto);
-    }
-);
+    mainContent.appendChild(boxProducto);
+    boxProducto.setAttribute("class","box-producto");
+
+    /* Caja para la imagen */
+    let boxImg = document.createElement("div");
+    boxProducto.appendChild(boxImg);
+    boxImg.setAttribute("class","box-img");
+    let imgAuto = document.createElement("img");
+    boxImg.appendChild(imgAuto);
+    imgAuto.setAttribute("src","img/pagani_Negro.jfif");
+    imgAuto.setAttribute("class","img-auto");
+
+    /* Caja para la informacion */
+    let boxInfo = document.createElement("div");
+    boxProducto.appendChild(boxInfo);
+    boxInfo.setAttribute("class","box-info");
+  /*   } */
+    let marca = this.document.createElement("label");
+    boxInfo.appendChild(marca);
+    let txtNodeMarca = document.createTextNode(auto1.marca + " " + auto1.modelo);
+    marca.appendChild(txtNodeMarca);
+});
